6e5bace41d794aa4cd3455ff6f1298794cffa075
Niyaj-Kumanali <iamniyazahmad777@gmail.com>
2023-03-19 22:55:16 +0530

    Java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,7 @@
 package Collection;
 
+import LeetCode.Node;
+
 public class MyLinkedList {
     private Node head;
     private Node tail;
@@ -65,6 +67,26 @@ public class MyLinkedList {
         Node node = new Node(val, tmp.next);
         tmp.next = node;
         size++;
+    }
+
+    public void insertUsingRecursion(int  val, int index, Node node) {
+        if (index == 1){
+            Node tmp = new Node(val);
+            tmp.next = node.next;
+            node.next = tmp;
+            size++;
+            return;
+        }
+        insertUsingRecursion(val, index-1, node.next);
+    }
+    private Node insertRec (int val, int index, Node node){
+        if (index == 0){
+            Node tmp = new Node(val, node);
+            size++;
+            return tmp;
+        }
+        node.next = insertRec(val, --index, node.next);
+        return node;
     }
 
     public int deleteFirst(){
@@ -138,6 +160,77 @@ public class MyLinkedList {
         tmp.next.val = val;
     }
 
+    public Node deleteDuplicates(Node head) {
+        if (head == null)
+            return head;
+        Node node = head;
+        while (node.next != null){
+            if (node.val == node.next.val){
+                node.next = node.next.next;
+            }
+            else {
+                node = node.next;
+            }
+        }
+        return node;
+    }
+
+    public static MyLinkedList merge(MyLinkedList list1, MyLinkedList list2){
+        Node f = list1.head;
+        Node s = list2.head;
+
+        MyLinkedList ans = new MyLinkedList();
+        while (f != null && s != null){
+            if (f.val < s.val){
+                ans.insertAtLast(f.val);
+                f = f.next;
+            } else {
+                ans.insertAtLast(s.val);
+                s = s.next;
+            }
+        }
+
+        while (f != null){
+            ans.insertAtLast(f.val);
+            f = f.next;
+        }
+
+        while (s != null){
+            ans.insertAtLast(s.val);
+            s = s.next;
+        }
+
+        return ans;
+    }
+
+    public void reverse(Node node){
+        if (node == tail){
+            head = tail;
+            return;
+        }
+
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+
+    public void reverseInplace(){
+        if (size < 2)
+            return;
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+
+        while (present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null)
+                next = next.next;
+        }
+        head = prev;
+    }
     public void clear(){
         Node node = new Node();
         head = node;
@@ -164,45 +257,74 @@ public class MyLinkedList {
 
     public static void main(String[] args) throws Exception {
         MyLinkedList list = new MyLinkedList();
-        list.insertAtFirst(1);
-        System.out.println(list.size);
-        list.insertAtFirst(2);
-        System.out.println(list.size);
-        list.insertAtFirst(3);
-        System.out.println(list.size);
-        list.insertAtFirst(4);
-        System.out.println(list.size);
-        list.insertAtLast(5);
-        System.out.println(list.size);
-        list.insertAtLast(6);
-        System.out.println(list.size);
-        list.insertAtLast(7);
-        System.out.println(list.size);
-        list.display();
-        System.out.println();
-        list.insertAt(9,1);
-        list.display();
-        System.out.println(list.size);
+//        list.insertAtFirst(1);
+//        System.out.println(list.size);
+//        list.insertAtFirst(2);
+//        System.out.println(list.size);
+//        list.insertAtFirst(3);
+//        System.out.println(list.size);
+//        list.insertAtFirst(4);
+//        System.out.println(list.size);
+//        list.insertAtLast(5);
+//        System.out.println(list.size);
+//        list.insertAtLast(6);
+//        System.out.println(list.size);
+//        list.insertAtLast(7);
+//        System.out.println(list.size);
+//        list.display();
+//        System.out.println();
+//        list.insertAt(9,1);
+//        list.display();
+//        System.out.println(list.size);
+//
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.size);
+//
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.size);
+//
+//        System.out.println(list.delete(1));
+//        list.display();
+//        System.out.println(list.size);
+//
+//        System.out.println(list.findValue(2));
+//        System.out.println();
+//        System.out.println(list.valueAt(3));
+//
+//        list.replace(3,0);
+//        list.display();
+//        list.insertUsingRecursion(7, 1, list.head);
+//        list.insertRec(8, 2, list.head);
+////        list.clear();
+//        list.display();
 
-        System.out.println(list.deleteFirst());
+        list.insertAtLast(1);
+        list.insertAtLast(1);
+        list.insertAtLast(1);
+        list.insertAtLast(2);
+        list.insertAtLast(4);
+        list.insertAtLast(4);
+//        list.display();
+//        list.deleteDuplicates(list.head);
+//        list.display();
+
+        MyLinkedList list2 = new MyLinkedList();
+        list2.insertAtLast(1);
+        list2.insertAtLast(1);
+        list2.insertAtLast(1);
+        list2.insertAtLast(2);
+        list2.insertAtLast(4);
+        list2.insertAtLast(4);
+        list2.insertAtLast(5);
+        list2.insertAtLast(6);
+
+//        MyLinkedList ans = MyLinkedList.merge(list, list2);
+//        ans.display();
         list.display();
-        System.out.println(list.size);
-
-        System.out.println(list.deleteLast());
+        list.reverse(list.head);
         list.display();
-        System.out.println(list.size);
 
-        System.out.println(list.delete(1));
-        list.display();
-        System.out.println(list.size);
-
-        System.out.println(list.findValue(2));
-        System.out.println();
-        System.out.println(list.valueAt(3));
-
-        list.replace(3,0);
-
-        list.clear();
-        list.display();
     }
 }
